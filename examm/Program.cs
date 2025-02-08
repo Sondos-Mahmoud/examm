414741912a2dc40b321f5be4a983c0d8b8d91d64
Sondos-Mahmoud <sondos.mohamed02@eng-st.cu.edu.eg>
2025-02-08 19:50:03 +0200

    exam

--- examm/Program.cs
+++ examm/Program.cs
@@ -18,30 +18,92 @@
 
             for (int i = 0; i < numberOfQuestions; i++)
             {
-                Console.WriteLine("# MCQ Question");
-                Console.WriteLine("Please Enter Question Body");
-                string body = Console.ReadLine();
-
-                Console.WriteLine("Please Enter Question Mark");
-                int mark = int.Parse(Console.ReadLine());
-
-                MCQQuestion mcqQuestion = new MCQQuestion($"Q{i + 1}", body, mark, 3);
-
-                for (int j = 0; j < 3; j++)
+                if (examType == 2) // Final Exam
                 {
-                    Console.WriteLine($"Please Enter Choice Number {j + 1}");
-                    string choice = Console.ReadLine();
-                    mcqQuestion.AddAnswer(new Answer(j + 1, choice));
+                    Console.WriteLine("Please Enter the Question Type (1 for True/False | 2 for MCQ)");
+                    int questionType = int.Parse(Console.ReadLine());
+
+                    if (questionType == 1) // True/False Question
+                    {
+                        Console.WriteLine("# True | False Question");
+                        Console.WriteLine("Please Enter Question Body");
+                        string body = Console.ReadLine();
+
+                        Console.WriteLine("Please Enter Question Mark");
+                        int mark = int.Parse(Console.ReadLine());
+
+                        TrueFalseQuestion tfQuestion = new TrueFalseQuestion($"Q{i + 1}", body, mark);
+
+                        Console.WriteLine("Please Enter the right Answer id (1 for True | 2 for False)");
+                        int rightAnswerId = int.Parse(Console.ReadLine());
+
+                        tfQuestion.AddAnswer(new Answer(1, "True"));
+                        tfQuestion.AddAnswer(new Answer(2, "False"));
+
+                        subject.Exam.AddQuestion(tfQuestion);
+                    }
+                    else if (questionType == 2) // MCQ Question
+                    {
+                        Console.WriteLine("# MCQ Question");
+                        Console.WriteLine("Please Enter Question Body");
+                        string body = Console.ReadLine();
+
+                        Console.WriteLine("Please Enter Question Mark");
+                        int mark = int.Parse(Console.ReadLine());
+
+                        MCQQuestion mcqQuestion = new MCQQuestion($"Q{i + 1}", body, mark, 3);
+
+                        for (int j = 0; j < 3; j++)
+                        {
+                            Console.WriteLine($"Please Enter Choice Number {j + 1}");
+                            string choice = Console.ReadLine();
+                            mcqQuestion.AddAnswer(new Answer(j + 1, choice));
+                        }
+
+                        Console.WriteLine("Please Enter the right Answer id");
+                        int rightAnswerId = int.Parse(Console.ReadLine());
+
+                        subject.Exam.AddQuestion(mcqQuestion);
+                    }
                 }
+                else if (examType == 1) 
+                {
+                    Console.WriteLine("# MCQ Question");
+                    Console.WriteLine("Please Enter Question Body");
+                    string body = Console.ReadLine();
 
-                Console.WriteLine("Please Enter the right Answer id");
-                int rightAnswerId = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please Enter Question Mark");
+                    int mark = int.Parse(Console.ReadLine());
 
-                subject.Exam.AddQuestion(mcqQuestion);
+                    MCQQuestion mcqQuestion = new MCQQuestion($"Q{i + 1}", body, mark, 3);
+
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.WriteLine($"Please Enter Choice Number {j + 1}");
+                        string choice = Console.ReadLine();
+                        mcqQuestion.AddAnswer(new Answer(j + 1, choice));
+                    }
+
+                    Console.WriteLine("Please Enter the right Answer id");
+                    int rightAnswerId = int.Parse(Console.ReadLine());
+
+                    subject.Exam.AddQuestion(mcqQuestion);
+                }
             }
 
-            subject.Exam.ShowExam();
+            Console.WriteLine("Do You Want To Start Exam (Y|N)");
+            string startExam = Console.ReadLine();
+
+            if (startExam.ToUpper() == "Y")
+            {
+                Console.Clear();
+                subject.Exam.ShowExam();
+            }
+            else
+            {
+                Console.WriteLine("Exam not started. Exiting...");
+            }
         }
     }
-    }
+}
 
